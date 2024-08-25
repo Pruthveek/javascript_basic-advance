a4987f66115e80add5d5e3f7c481e494a4abb731
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-08-25 13:36:10 +0000

    1

---
+++ basic/01_installation.js
@@ -0,0 +1,6 @@
+// 1. Download and install a node.js from (https://nodejs.org/en).
+// 2. open vscode and open terminal andwrite command (node -v) and enter if it give output like (ex. v22.6.0) then it's good otherwise check node is proper installed or not.
+// 3. create a new file (demo.js) and write a bellow code in it.
+// 4. write a command (node demo.js) in terminal and check code run successfully or not? 
+
+console.log("Javascript basic to advance")
