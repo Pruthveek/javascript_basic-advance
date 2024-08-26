cd28a36865624682d2311eb76e52bee796e2e200
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-08-26 06:15:00 +0000

    3

--- basic/03_datatypes.js
+++ basic/03_datatypes.js
@@ -1,4 +1,6 @@
 "Use strict";//treat all js code as newer version
+// 1. Primitive - String, Number, Boolean, null, undefined, Symbol, BigInt
+// 2. Nonprimitive (Reference) - Array, Object, function
 //alert(3+3)
 //We are using nodejs, not browser
 
@@ -7,7 +9,7 @@ console.log(7+" Pruthveek")
 
 let name="Pruthveek" //string
 let age=32 //number
-let mobileNo=7433993937 //bigint
+let mobileNo=7433993937n //bigint
 let active=false //boolean
 let temp=null //null but it's type is object
 let car; //nudefined
@@ -17,3 +19,15 @@ let car; //nudefined
 
 console.log(typeof temp) //object
 console.log(typeof car) //undefined
+console.log(typeof mobileNo)
+console.log(mobileNo)
+
+const colour=["red", "yellow", "green", "blue", "orenge"]//array
+let userInfo={
+    name:"Pruthveek",
+    age:32,
+    city:"Ahmedabad",
+}//object
+const hellofunction=function(){
+    console.log("Hello this is a function")
+}//this is a function which is stored in variable
