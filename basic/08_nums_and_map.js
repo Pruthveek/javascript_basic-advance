df3590872759ed9ebdff6c656aa880e9990c8a80
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-08-30 15:27:06 +0000

    7

--- basic/08_nums_and_map.js
+++ basic/08_nums_and_map.js
@@ -24,8 +24,8 @@ console.log(Math.min(4, 5, 6, 2, 8));
 console.log(Math.max(4, 5, 6, 2, 8));
 
 console.log(Math.random())//it always give value beetween 0 and 1
-console.log(math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
 
-const min =1
-const max=6
-Math.random()
+const min = 10
+const max = 20
+Math.random(Math.floor(Math.random() * (max - min + 1)) + min)
