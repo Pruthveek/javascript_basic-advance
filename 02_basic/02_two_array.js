4fb300f9cc4dd87523d454e73ad5c56108406763
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-08-31 07:28:09 +0000

    2.2

---
+++ 02_basic/02_two_array.js
@@ -0,0 +1,11 @@
+let marvelHeros=["ironman", "Spiderman", "Thor", "Loki", "Hulk"]
+let dcHeros=["Batman", "Superman", "Blackadam"]
+
+console.log(`MARVAL heros : ${marvelHeros}`);
+console.log(`DC heros : ${dcHeros}`);
+
+let allHeros=marvelHeros.concat(dcHeros)
+console.log(`All heros : ${allHeros}`)
+
+console.log(`MARVAL heros : ${marvelHeros}`);
+console.log(`DC heros : ${dcHeros}`);
