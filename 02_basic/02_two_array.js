85b595864979d3ca62fec52c957351c56025a8bd
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-09-02 13:18:17 +0000

    arr

--- 02_basic/02_two_array.js
+++ 02_basic/02_two_array.js
@@ -4,9 +4,22 @@ let dcHeros=["Batman", "Superman", "Blackadam"]
 console.log(`MARVAL heros : ${marvelHeros}`);
 console.log(`DC heros : ${dcHeros}`);
 
-let allHeros=marvelHeros.concat(dcHeros)
+//let allHeros=marvelHeros.concat(dcHeros)
+let allHeros=[...marvelHeros, ...dcHeros]
 console.log(`All heros : ${allHeros}`)
 
 console.log(`MARVAL heros : ${marvelHeros}`);
 console.log(`DC heros : ${dcHeros}`);
 
+const anotherarray=[1,2,3,[4,5],6,7,[8,9,[10,11,12,13]]]
+console.log(anotherarray)
+console.log(anotherarray.flat(Infinity))
+console.log(anotherarray)
+console.log(`this is array inside array : ${anotherarray}`)
+console.log(Array.from("Pruthveek"))
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1, score2, score3));
