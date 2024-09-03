5805bd1ee25c6beb2b6bf42f35e608b7f52f18a1
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-09-03 08:47:06 +0000

    scopes

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,29 @@
+//var
+console.log(`var`)
+var varVariable=10
+if(varVariable){
+    varVariable=20
+    var varVariable2=40
+}
+console.log(varVariable)
+console.log(varVariable2)
+
+//let
+console.log(`let`)
+let letVariable=10
+if(letVariable){
+    letVariable=20
+    let letVariable2=40
+}
+console.log(letVariable)
+//console.log(letVariable2) //this variable give you error
+
+// const
+console.log(`const`)
+const constVariable=10
+if (constVariable){
+    //constVariable=20
+    const constVariable2=40
+}
+console.log(constVariable)
+//console.log(constVariable2)
