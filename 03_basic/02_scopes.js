8a8576fd1bec64463717228a60477b3faabaafe6
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-09-05 05:29:47 +0000

    scope

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -21,9 +21,44 @@ console.log(letVariable)
 // const
 console.log(`const`)
 const constVariable=10
+//constVariable=50
 if (constVariable){
     //constVariable=20
     const constVariable2=40
 }
 console.log(constVariable)
 //console.log(constVariable2)
+
+//Scop of function
+function one(){
+    const userName="Pruthveek"
+    function two(){
+        const website="Github"
+        console.log(userName);
+
+    }
+    //console.log(website); -> it can't acesses out of box
+    two()
+}
+one()
+
+// scope of if
+if(true){
+    const userName="Pruthveek"
+    if(userName === "Pruthveek"){
+        const website="Github"
+        console.log(`${userName} on a ${website}`)
+    }
+    //console.log(website);
+}
+//console.log(userName);
+
+console.log(addNumber(2,5,7))
+function addNumber(num1,num2,num3){
+    return num1+num2+num3
+}
+
+// console.log(addTwoNumber(2,5))
+// const addTwoNumber =function(num1,num2){
+//     return num1+num2
+// }
