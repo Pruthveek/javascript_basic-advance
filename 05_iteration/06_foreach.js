06f7aa576504d05e58ddc436b948cb46675c1b78
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-09-14 13:46:10 +0000

    filter

--- 05_iteration/06_foreach.js
+++ 05_iteration/06_foreach.js
@@ -1,36 +1,36 @@
 //forEach
-const coding=["js", "ruby", "java", "python", "cpp"]
-coding.forEach(function(val){
+const coding = ["js", "ruby", "java", "python", "cpp"]
+coding.forEach(function (val) {
     console.log(val);
 })
 
 //arow function
 console.log("fatch data from array using forEatch with arrow function");
-coding.forEach((val)=>{console.log(val)});
+coding.forEach((val) => { console.log(val) });
 
-function printMe(item){
+function printMe(item) {
     console.log(item);
 }
 coding.forEach(printMe);
 
-coding.forEach((item,index,arr)=>{
-    console.log(item,index,arr);
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
 })
 
-const myCoding=[
+const myCoding = [
     {
-        languageName:"javascript",
-        languageFilename:"js"
+        languageName: "javascript",
+        languageFilename: "js"
     },
     {
-        languageName:"java",
-        languageFilename:"java"
+        languageName: "java",
+        languageFilename: "java"
     },
     {
-        languageName:"python",
-        languageFilename:"py"
+        languageName: "python",
+        languageFilename: "py"
     }
 ]
-myCoding.forEach((item)=>{
-    console.log(item.languageName,"=>",item.languageFilename);
+myCoding.forEach((item) => {
+    console.log(item.languageName, "=>", item.languageFilename);
 })
---
+++ 05_iteration/07_filter.js
@@ -0,0 +1,8 @@
+
+//in this output will be undifined because foreach function can not return any value so we use filter
+// const coding = ["js", "ruby", "java", "python", "cpp"];
+// const values = coding.forEach((value)=>{
+//     console.log(value);
+//     return value;
+// })
+// console.log(values);
