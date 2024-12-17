de645039fe5cda6e56c64802ae264c8590a9fffb
Pruthveek <88721137+Pruthveek@users.noreply.github.com>
2024-12-17 07:08:51 +0000

    eev

--- 00_basic/eventListeners/event.js
+++ 00_basic/eventListeners/event.js
@@ -1,3 +1,7 @@
-document.getElementById("clickme").addEventListener("click",function(){
-    console.log("button clicked ");
-})
+function eventlisteners(){
+	counter=0;
+	document.getElementById("clickme").addEventListener("click",function(){
+    console.log("button clicked ",++counter , " times");
+	})	
+}
+eventlisteners();
